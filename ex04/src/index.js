4c27276416d7b55f8630ed8fa1f3baaf0be443b6
ismailrascic <ismaily.rascic@gmail.com>
2021-03-26 22:07:25 +0100

    react

---
+++ ex04/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM, { render } from 'react-dom';
+import './index.css';
+
+
+
+class App extends React.Component{ render(){return(  <div className="App"><h1>Hello,Padawans</h1></div>  );}}
+ReactDOM.render(<App />,document.getElementById('root'));
